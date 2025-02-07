f06f284a66fc402642ef3f3d75ded7548646d927
James Swent <jake@osprey21.com>
2025-02-07 05:59:37 +0000

    feat(lsp): Force eslint formatting

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = { eslint = {} },
+      setup = {
+        eslint = function()
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      opts.formatters_by_ft = vim.tbl_deep_extend("force", opts.formatters_by_ft, {
+        java = { "google-java-format" },
+      })
+    end,
+  },
+}
