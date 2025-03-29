93ebae86add5759d09f6bebb9dfb38b6b45bc8fa
James Swent <jake@osprey21.com>
2025-03-29 00:35:46 +0000

    feat(format): Add rustfmt formatter

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -21,6 +21,7 @@ return {
     opts = function(_, opts)
       opts.formatters_by_ft = vim.tbl_deep_extend("force", opts.formatters_by_ft, {
         java = { "google-java-format" },
+        rust = { "rustfmt" },
       })
     end,
   },
