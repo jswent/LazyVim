af869ad9d8c87504cf2f3761f4e638efbd39730f
James Swent <jake@osprey21.com>
2025-05-28 15:22:57 -0400

    fix: Depracted nvim_buf_get_option

--- lua/jswent/functions.lua
+++ lua/jswent/functions.lua
@@ -52,7 +52,7 @@ function M.isempty(s)
 end
 
 function M.get_buf_option(opt)
-  local status_ok, buf_option = pcall(vim.api.nvim_buf_get_option, 0, opt)
+  local status_ok, buf_option = pcall(vim.api.nvim_get_option_value, opt, { buf = 0 })
   if not status_ok then
     return nil
   else
@@ -62,7 +62,7 @@ end
 
 function M.smart_quit()
   local bufnr = vim.api.nvim_get_current_buf()
-  local modified = vim.api.nvim_buf_get_option(bufnr, "modified")
+  local _, modified = pcall(vim.api.nvim_get_option_value, "modified", { buf = bufnr })
   if modified then
     vim.ui.input({
       prompt = "You have unsaved changes. Quit anyway? (y/n) ",
