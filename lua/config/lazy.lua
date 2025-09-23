1bbb50d8f7e1888f3bd45b2b89f4c09aa05b9a70
James Swent <jake@osprey21.com>
2025-09-23 17:57:59 +0100

    chore: Migrate LazyVim extras to lazyvim.json

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,19 +19,6 @@ local opts = {
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import any extras modules here
-    { import = "lazyvim.plugins.extras.coding.yanky" },
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.tailwind" },
-    { import = "lazyvim.plugins.extras.lang.rust" },
-    { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.lang.markdown" },
-    { import = "lazyvim.plugins.extras.lang.clangd" },
-    { import = "lazyvim.plugins.extras.lang.toml" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.dap.core" },
     -- import/override with your plugins
     { import = "plugins" },
     { import = "jswent.lang.sourcekit" },
--- lua/jswent/lang/sourcekit.lua
+++ lua/jswent/lang/sourcekit.lua
@@ -5,7 +5,9 @@ if lsp_installed == 0 or vim.g.jswent_sourcekit_enabled == false then
 end
 
 return {
-
+  recommended = {
+    ft = "swift",
+  },
   {
     "neovim/nvim-lspconfig",
     opts = function()
