a03c8e398ccaf49c105aa9db984b14096271c9f9
James Swent <jake@osprey21.com>
2025-10-12 14:38:47 +0100

    extras: move essence config to extras.lang

---
+++ lua/jswent/extras/lang/essence.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "jswent/essence.nvim",
+    -- ft = "essence", -- for lazy load on filetype
+    lazy = false,
+    ---@module "essence"
+    ---@type EssenceConfigUser
+    opts = {
+      conceal = false,
+    },
+  },
+}
