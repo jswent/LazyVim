0fefb588074e844dc25c31af079f6ed13dbe47bb
James Swent <jake@osprey21.com>
2025-11-25 15:58:10 +0000

    lsp: remove eslint on_attach pending refactor

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,27 +1,27 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = function(_, opts)
-      ---@class PluginLspOpts
-      local userOpts = {
-        servers = {
-          eslint = {},
-        },
-        setup = {
-          eslint = function()
-            require("lazyvim.util").lsp.on_attach(function(client)
-              if client.name == "eslint" then
-                client.server_capabilities.documentFormattingProvider = true
-              elseif client.name == "tsserver" then
-                client.server_capabilities.documentFormattingProvider = false
-              end
-            end)
-          end,
-        },
-      }
-      return vim.tbl_deep_extend("force", opts, userOpts)
-    end,
-  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = function(_, opts)
+  --     ---@class PluginLspOpts
+  --     local userOpts = {
+  --       servers = {
+  --         eslint = {},
+  --       },
+  --       setup = {
+  --         eslint = function()
+  --           require("lazyvim.util").lsp.on_attach(function(client)
+  --             if client.name == "eslint" then
+  --               client.server_capabilities.documentFormattingProvider = true
+  --             elseif client.name == "tsserver" then
+  --               client.server_capabilities.documentFormattingProvider = false
+  --             end
+  --           end)
+  --         end,
+  --       },
+  --     }
+  --     return vim.tbl_deep_extend("force", opts, userOpts)
+  --   end,
+  -- },
   {
     "stevearc/conform.nvim",
     opts = function(_, opts)
@@ -29,7 +29,6 @@ return {
       ---@type conform.setupOpts
       local userOpts = {
         formatters_by_ft = {
-          java = { "google-java-format" },
           rust = { "rustfmt" },
         },
       }
